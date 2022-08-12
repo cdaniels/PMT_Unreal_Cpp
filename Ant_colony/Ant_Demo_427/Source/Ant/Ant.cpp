83ebc3193179dd93ce284a7572ef3d2eca0aa8c3
maitry-trivedi <15bit063@nirmauni.ac.in>
2022-08-12 14:45:33 -0400

    SearchAlgo
    Astar and Antcolony

---
+++ Ant_colony/Ant_Demo_427/Source/Ant/Ant.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Ant.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Ant, "Ant" );
